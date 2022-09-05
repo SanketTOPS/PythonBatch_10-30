9dc42c090d99e60801c8f1aac937cfc4dcb65052
Django-TOPS <djangotestmail2021@gmail.com>
2022-09-05 11:26:40 +0530

    List

---
+++ Module-2_Collection/list.py
@@ -0,0 +1,65 @@
+tech=['Python','JAVA','PHP','React','HTML']
+
+"""print(tech)
+print(tech[0])
+print(tech[-1])
+print(tech[1:4])
+print(tech[2:])
+print(tech[:3])
+"""
+
+"""print(len(tech))
+print(tech)
+tech[1]='Android'
+print(tech)"""
+
+# ----------------------------------------- #
+
+
+
+"""for i in tech:
+    print(i)
+"""
+#Index[0]=Python
+#Index[1]=JAVA
+
+#print(tech.index('Python'))
+#print(tech.index('PHP'))
+
+"""for i in tech:
+    print(f"Index:{tech.index(i)} = {i}")"""
+
+"""n=0
+for i in tech:
+    print(f"Index[{n}] = {i}")
+    n+=1"""
+
+"""if 'Python' in tech:
+    print("Yes..")
+else:
+    print("Noo")"""
+
+#print(tech)
+#tech.append("Node")
+#tech.insert(2,'Kotlin')
+#tech.pop()
+#tech.pop(1)
+#tech.clear()
+#tech.remove('HTML')
+#del tech[0]
+#del tech
+print(tech)
+
+#newlist=tech.copy()
+#print(newlist)
+
+"""newlist=["C","C++","DS","C#"]
+print(newlist)
+#print(tech+newlist)
+tech.extend(newlist)
+print(tech)
+"""
+
+#tech.sort()
+tech.reverse()
+print(tech)
