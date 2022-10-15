92b37e073c9b80a2a038420e27bf08c53e7889b0
Django-TOPS <djangotestmail2021@gmail.com>
2022-10-15 10:06:25 +0530

    Select + Delete Operations

---
+++ Module-3_OOPS/class_create.py
@@ -0,0 +1,13 @@
+class student:
+    stid=12
+    stnm='Nirav'
+
+    def getdata(self):
+        print("This is OOP.")
+
+
+# Object
+st=student()
+print(st.stid)
+print(st.stnm)
+st.getdata()
---
+++ Module-3_OOPS/class_func_arg.py
@@ -0,0 +1,8 @@
+class student:
+    def getdata(self,stid,stnm):
+        print("Student ID:",stid)
+        print("Student Name:",stnm)
+
+
+st=student()
+st.getdata(1,'Mitesh')
